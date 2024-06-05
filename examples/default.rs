b6989cb0b4c8b659a0040bca06f13a2c73a01ce6
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2024-06-05 15:03:44 +0100

    Add examples

---
+++ examples/default.rs
@@ -0,0 +1,24 @@
+use selthi::Selthi;
+
+fn main() {
+    let options: Vec<&str> = vec![
+        "Rust",
+        "C",
+        "C++",
+        "Javascript",
+        "Java",
+        "C#",
+        "Python",
+        "Haskell",
+        "Lisp",
+        "HTML",
+    ];
+
+    let ans: Option<&str> =
+        Selthi::new("What's your favorite programming language?", options).prompt();
+
+    match ans {
+        Some(language) => println!("{} rocks!", language),
+        None => println!("There was an error, please try again"),
+    }
+}
---
+++ examples/images.rs
@@ -0,0 +1,20 @@
+use selthi::Selthi;
+
+fn main() {
+    let options: Vec<&str> = vec!["Linux", "Windows", "macOS"];
+    let images: Vec<&str> = vec![
+        "./examples/images/linux.png",
+        "./examples/images/windows.png",
+        "./examples/images/macos.png",
+    ];
+
+    let ans: Option<&str> = Selthi::new("What's your favorite operating system?", options)
+        .with_images(images)
+        .without_help_message()
+        .prompt();
+
+    match ans {
+        Some(os) => println!("{} is a good choice!", os),
+        None => println!("There was an error, please try again"),
+    }
+}
